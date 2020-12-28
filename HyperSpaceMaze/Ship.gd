0b1aca9cc44ac63300761a31afaa7180874f48bc
ShooperGames <66393946+ShooperGames@users.noreply.github.com>
2020-12-28 09:58:00 -0500

    Add files via upload

--- HyperSpaceMaze/Ship.gd
+++ HyperSpaceMaze/Ship.gd
@@ -5,9 +5,11 @@ extends Node2D
 # var a = 2
 # var b = "text"
 export var loc = {'x':0, 'y':0}
+var upTexture = load("res://HyperSpaceMaze/Ship_Rest_Up.png")
+var rightTexture = load("res://HyperSpaceMaze/Ship_Rest_Right.png")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	loc = {'x':0, 'y':0}
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
